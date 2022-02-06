2b8b955441a44f91b827f6d75ddf27e962da3dd1
Michaelreforged <michaelcheung.cheme@gmail.com>
2022-02-06 04:23:22 -0700

    optimized prune, set limit to 50 due to fetch limit of 100"

--- commands/prune.js
+++ commands/prune.js
@@ -7,42 +7,32 @@ module.exports = {
 		.setDescription('Prunes bot messages')
 		.addIntegerOption(option =>
 			option.setName('input')
-				.setDescription('Number of messages to prune, else prune 10'),
+				.setDescription('Number of bot messages to try prune, else prune 10, cannot exceed 50'),
 		),
 	async execute(interaction) {
 		// console.log(interaction.channel.messages.purge);
-
-		const botMessages = async () => {
-			const bmsgs = [];
-			await interaction.channel.messages.fetch({ limit:100 }).then(messages => {
-				messages.filter(m => m.author.id === clientId)
-					.forEach(m => {
-						bmsgs.push(m);
-					});
+		await interaction.channel.messages.fetch({ limit:100 })
+			.then(messages => {
+				const b = [];
+				messages.filter(m => m.author.id === clientId).forEach(msg => {
+					b.push(msg);
+				});
+				return b;
+			})
+			.then(b => {
+				if (interaction.options._hoistedOptions[0]) {
+					const check = (interaction.options._hoistedOptions[0].value) > 50 ? true : false;
+					const limit = b.splice(0, check ? 50 : interaction.options._hoistedOptions[0].value);
+					interaction.channel.bulkDelete(limit);
+					check ? interaction.reply({ content: 'Cannot prune past 50, Pruning last 50 bot messages', ephemeral: true }) : interaction.reply({ content: `Pruning last ${interaction.options._hoistedOptions[0].value}`, ephemeral: true });
+				}
+				else {
+					const limit = b.slice(0, 10);
+					interaction.channel.bulkDelete(limit);
+					interaction.reply({ content: 'Pruning last 10 bot messages', ephemeral: true });
+				}
 			},
 			);
-			return bmsgs;
-		};
-		if (interaction.options._hoistedOptions[0]) {
-			const b = botMessages();
-			setTimeout(() => {
-				b.then((msg) => {
-					const limit = msg.splice(0, interaction.options._hoistedOptions[0].value);
-					interaction.channel.bulkDelete(limit);
-				});
-			}, 1000);
-			await interaction.reply({ content: `Pruning last ${interaction.options._hoistedOptions[0].value}`, ephemeral: true });
-		}
-		else {
-			const b = botMessages();
-			setTimeout(() => {
-				b.then((msg) => {
-					const limit = msg.slice(0, 9);
-					interaction.channel.bulkDelete(limit);
-				});
-			}, 1000);
-			await interaction.reply({ content: 'Pruning last 10 bot messages', ephemeral: true });
-		}
 	},
 };
 
